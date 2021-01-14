06cd417512cdfbead5df9a53c71f54e3c48a5d5b
雷斌 <1846102760@qq.com>
2021-01-14 21:21:42 +0800

    提交一下新git的代码操作

---
+++ src/test/java/com/tencent/MyFun.java
@@ -0,0 +1,14 @@
+package com.tencent;
+
+/**
+ * @author 雷斌
+ * @version V1.0
+ * @Package com.tencent
+ * @date 2021/1/10 22:35
+ * @Copyright ©  爱讯技术有限公司
+ */
+
+
+public interface MyFun {
+    public Integer getValue(Integer num);
+}
---
+++ src/test/java/com/tencent/test.java
@@ -0,0 +1,25 @@
+package com.tencent;
+
+import org.junit.Test;
+
+/**
+ * @author 雷斌
+ * @version V1.0
+ * @Package com.tencent
+ * @date 2021/1/10 22:23
+ * @Copyright ©  爱雷信息技术有限公司
+ */
+public class test {
+
+    //需求，对一个数进行运算
+    @Test
+    public void test1(){
+        Integer num = operation(100,x -> x * x);
+        System.out.println(num);
+    }
+
+    public Integer operation(Integer num,MyFun mf){
+        return mf.getValue(num);
+    }
+
+}
